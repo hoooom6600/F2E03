2395d17a8448ac4302514184e660f95e3d1a8c27
hoooom6600 <hoooom6600@gmail.com>
2025-10-22 14:19:12 +0800

    修改: 將 fetch 挪到全域宣告以提升效能、改變變數名稱方便辨識； 新增: 不輸入代表搜尋全部資料

--- HW/youbike/app.js
+++ HW/youbike/app.js
@@ -8,63 +8,77 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const input = document.querySelector("input");
   const ul = document.querySelector("ul");
 
+  // 為提升效能，避免點擊一次按鈕就要重新 fetch，所以先在 global fetch
+  const items = fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((item) => {
+      return item;
+    });
+
   button.addEventListener("click", (e) => {
     // 取消 button 預設提交 form 行為，否則頁面會重整（URL 後面多一個問號）
     e.preventDefault();
 
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((stations) => {
-        // 如果 ul 內有東西，清空。 ul 內的元素來自前次的檢索
-        if (ul.childNodes.length > 0) {
-          ul.innerHTML = "";
-        }
+    items.then((stations) => {
+      // 如果 ul 內有東西，清空。 ul 內的元素來自前次的檢索
+      if (ul.childNodes.length > 0) {
+        ul.innerHTML = "";
+      }
 
-        // 取得符合搜尋條件（限路名）的站點（陣列）;
-        const value = input.value;
-        const stationsMatch = stations.filter((station) => {
-          return station.ar.includes(value);
-        });
-
-        // 在頁面顯示搜尋結果
-        // 不允許空字串
-        if (value.trim().length == 0) {
-          const empty = document.createElement("p");
-          empty.className = "empty";
-          empty.textContent = "請輸入關鍵字";
-          ul.appendChild(empty);
-          return;
-        }
-
-        // 沒有符合的搜尋條件
-        if (stationsMatch.length == 0) {
-          const empty = document.createElement("p");
-          empty.className = "empty";
-          empty.textContent = "查無搜尋結果";
-          ul.appendChild(empty);
-          return;
-        }
-
-        // 關鍵字通過
-        stationsMatch.forEach((stationMatch) => {
-          // 站點名稱
-          const newLiStationName = document.createElement("li");
-          newLiStationName.className = "station-name";
-
-          newLiStationName.innerHTML =
-            bikeIcon + stationMatch.sna.replace("YouBike2.0_", "");
-
-          // 站點路名
-          const newPAddress = document.createElement("p");
-          newPAddress.textContent = stationMatch.ar;
-          newPAddress.className = "station-address";
-
-          // ul 內新增元素
-          newLiStationName.appendChild(newPAddress);
-          ul.appendChild(newLiStationName);
-        });
+      // 取得符合搜尋條件（限路名）的站點（陣列）;
+      const value = input.value;
+      const stationsMatch = stations.filter((station) => {
+        return station.ar.includes(value);
       });
+
+      // 在頁面顯示搜尋結果
+      // 不允許空字串，但沒有任何輸入代表搜尋全部站點
+      if (value != "" && value.trim().length == 0) {
+        const empty = document.createElement("p");
+        empty.className = "empty";
+        empty.textContent = "請輸入關鍵字";
+        ul.appendChild(empty);
+        return;
+      }
+
+      // 沒有符合的搜尋條件
+      if (stationsMatch.length == 0) {
+        const empty = document.createElement("p");
+        empty.className = "empty";
+        empty.textContent = "查無搜尋結果";
+        ul.appendChild(empty);
+        return;
+      }
+
+      // 關鍵字通過
+      stationsMatch.forEach((stationMatch) => {
+        // 站點名稱
+        const list = document.createElement("li");
+        const stationNameBox = document.createElement("span");
+        stationNameBox.className = "station-name";
+        stationNameBox.innerHTML = `${bikeIcon}${stationMatch.sna.replace(
+          "YouBike2.0_",
+          ""
+        )}`; // 為了能正確解析 icon，所以用 innerHTML
+
+        // 站點路名
+        const addressBox = document.createElement("p");
+        addressBox.className = "station-address";
+        addressBox.textContent = stationMatch.ar;
+
+        // 可租借數量
+        const availableCountBox = document.createElement("span");
+        availableCountBox.className = "available-count";
+        availableCountBox.textContent = `(${stationMatch.available_rent_bikes})`;
+
+        // ul 內新增 list，list 內插入站點名稱、可租借數量、站點路名
+        list.appendChild(stationNameBox);
+        list.appendChild(availableCountBox);
+        list.appendChild(addressBox);
+        ul.appendChild(list);
+      });
+    });
   });
 });
