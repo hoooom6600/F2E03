1532f95e72cbbccbe0f91a4e39fa69a91a5c97f8
hoooom6600 <hoooom6600@gmail.com>
2025-10-27 14:24:18 +0800

    修改: 排版編輯；新增: export / import 其他玩法

--- course-demo/JS/2025-10-27/module-main.js
+++ course-demo/JS/2025-10-27/module-main.js
@@ -1,4 +1,7 @@
-import { aa, xyz } from "./module-aa.js";
+import { aa, xyz, name } from "./module-aa.js";
+import abc from "./module-aa.js";
 aa();
 console.log(aa);
 console.log(xyz);
+console.log(name);
+console.log(abc);
