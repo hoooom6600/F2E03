1b67c7955419630b8acc47f14d8fa210fe306cd4
hoooom6600 <hoooom6600@gmail.com>
2025-10-30 12:28:20 +0800

    修改: 糖果第 3 題實作，將函數簡化，分成合格與不合格兩組，最後用 ... 展開串接

--- HW/candy/03.js
+++ HW/candy/03.js
@@ -7,14 +7,11 @@ let list = [false, 1, 0, -1, 2, 0, 1, 3, "a"];
 function moveZerosToEnd(arr) {
   // 程式碼寫在這裡
   const target = 0; // 定義標的，依題目要求而異。本題為 0
-  const targetCount = arr.filter((element) => element === target).length; // 嚴格相等，否則 false 會視為 0
+  const validGroup = arr.filter((element) => element !== target); // 嚴格相等，否則 false 會視為 0
+  const targetGroup = arr.filter((element) => element === target); // 嚴格相等，否則 false 會視為 0
 
-  // 把 0 去除，然後在陣列末端依 0 原本出現的數量補上
-  arr = arr.filter((element) => element !== 0);
-  for (let i = 0; i < targetCount; i++) {
-    arr.push(target);
-  }
-  return arr;
+  // 把 validGroup 和 targetGroup 陣列展開然後串接，變成單層陣列
+  return [...validGroup, ...targetGroup];
 }
 
 let result = moveZerosToEnd(list);
