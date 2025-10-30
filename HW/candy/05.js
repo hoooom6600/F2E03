fe6c48550935baa4a57fb6c8a00812a6551fc150
hoooom6600 <hoooom6600@gmail.com>
2025-10-30 20:27:17 +0800

    新增: 糖果第 5 題實作，並做第 1 次作答繳交

--- HW/candy/05.js
+++ HW/candy/05.js
@@ -5,16 +5,19 @@
 function squareDigits(num) {
   // 實作寫在這裡
   const pow = 2; // 可依題目要求改變指數
-  const stringNum = [];
+  const digits = [];
 
   for (let i = 0; i < num.toString().length; i++) {
-    stringNum.push(num.toString()[i]);
+    if (isNaN(num.toString()[i])) {
+      return "可接受數值和字串型態的數字，但拒絕有任一字元非數字";
+    }
+    // 把參數的位數逐一轉成字串
+    digits.push(num.toString()[i]);
   }
-  stringNum = stringNum.map((digit) => Math.pow(digit, pow));
-  console.log(stringNum);
-  //   return stringNum.map((digit) => Math.pow(digit, pow));
+  // 為開發嚴謹，不依賴 JS 強制轉型做數學，先手動轉成 number 資料型態，並回傳 number 格式
+  return Number(digits.map((digit) => Math.pow(Number(digit), pow)).join(""));
 }
 
 console.log(squareDigits(3212)); // 印出 9414
-// console.log(squareDigits(2112)); // 印出 4114
-// console.log(squareDigits(387)); // 印出 96449
+console.log(squareDigits(2112)); // 印出 4114
+console.log(squareDigits(387)); // 印出 96449
