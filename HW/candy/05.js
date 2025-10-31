01ca434c41bcc023074cfdebf39c68a23f60cc07
hoooom6600 <hoooom6600@gmail.com>
2025-10-31 23:18:14 +0800

    改善: 糖果第 5 題實作，盡量使用陣列內建方法。並做第 2 次作答繳交

--- HW/candy/05.js
+++ HW/candy/05.js
@@ -5,17 +5,18 @@
 function squareDigits(num) {
   // 實作寫在這裡
   const pow = 2; // 可依題目要求改變指數
-  const digits = [];
 
-  for (let i = 0; i < num.toString().length; i++) {
-    if (isNaN(num.toString()[i])) {
-      return "可接受數值和字串型態的數字，但拒絕有任一字元非數字";
-    }
-    // 把參數的位數逐一轉成字串
-    digits.push(num.toString()[i]);
+  // 將參數轉成字串形式然後展開為陣列其中一個元素
+  // String is iterable
+  const elements = [...num.toString()];
+  const isOnlyDigits = elements.every(
+    (currentElement) => !isNaN(Number(currentElement))
+  );
+  if (!isOnlyDigits) {
+    return "可接受數值和字串型態的數字，但拒絕有任一字元非數字";
   }
   // 為開發嚴謹，不依賴 JS 強制轉型做數學，先手動轉成 number 資料型態，並回傳 number 格式
-  return Number(digits.map((digit) => Math.pow(Number(digit), pow)).join(""));
+  return Number(elements.map((digit) => Math.pow(Number(digit), pow)).join(""));
 }
 
 console.log(squareDigits(3212)); // 印出 9414
