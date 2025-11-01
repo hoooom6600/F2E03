a5019254d072f208cd994e52801dcb5d6c996611
hoooom6600 <hoooom6600@gmail.com>
2025-11-01 18:15:12 +0800

    新增: 糖果第 7 題實作，並做第 1 次作答繳交

---
+++ HW/candy/07.js
@@ -0,0 +1,19 @@
+// 編號：CANDY-007
+// 程式語言：JavaScript
+// 題目：在某個數字陣列裡，可能藏有某個不合群的奇數或偶數，試著找出它！
+
+function findSomeDifferent(numbers) {
+  // 實作寫在這裡
+  // 以偶數為判斷基準
+  const evens = numbers.filter((num) => num % 2 === 0);
+
+  // 唯一奇數
+  // 試著將單行區塊簡寫成表達式
+  if (evens.length > 1) return numbers.find((number) => number % 2 !== 0);
+
+  // 唯一偶數
+  return evens[0];
+}
+
+console.log(findSomeDifferent([2, 4, 0, 100, 4, 11, 2602, 36])); // 印出 11
+console.log(findSomeDifferent([160, 3, 1719, 19, 11, 13, -21])); // 印出 160
