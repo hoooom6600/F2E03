6092339e78dfc0f6a556ce44e67faf31b17a2b3a
hoooom6600 <hoooom6600@gmail.com>
2025-11-03 21:19:39 +0800

    修改: 糖果第 8 題實作，加入大寫判斷。新手寫法待改善，故尚未正式繳交

--- HW/candy/08.js
+++ HW/candy/08.js
@@ -10,9 +10,24 @@ function highestScoreWord(input) {
   let highestScore = 0;
   let currentScore = 0;
   let highestWord = "";
+
+  const hasUppercase = words.some((word) => {
+    for (let i = 0; i < word.length; i++) {
+      // a's ANSI == 97
+      // ASCII code number: lowercase > uppercase
+      if (word.charCodeAt(i) - 97 < 0) {
+        return true;
+      }
+    }
+  });
+
+  if (hasUppercase) {
+    return "拒絕大寫";
+  }
+
   words.forEach((word) => {
     for (let i = 0; i < word.length; i++) {
-      currentScore += word.charCodeAt(i);
+      currentScore += word.charCodeAt(i) - 96;
     }
     if (currentScore > highestScore) {
       highestScore = currentScore;
