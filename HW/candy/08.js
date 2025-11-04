0e9e37923869256fe6b4498b952b84f296bd9284
hoooom6600 <hoooom6600@gmail.com>
2025-11-04 15:06:14 +0800

    修改: 糖果第 8 題大寫判斷邏輯，用正則表達簡化程式碼。並做第 1 次繳交

--- HW/candy/08.js
+++ HW/candy/08.js
@@ -6,27 +6,18 @@
 
 function highestScoreWord(input) {
   // 實作寫在這裡
+  // 練習正則表達式
+  if ([...input].filter((words) => words.match(/[A-Z]/)).length) {
+    return "拒絕任何大寫";
+  }
   const words = input.split(" ");
   let highestScore = 0;
   let highestWord = "";
 
-  const hasUppercase = words.some((word) => {
-    for (let i = 0; i < word.length; i++) {
-      // a's ANSI == 97
-      // ASCII code number: lowercase > uppercase
-      if (word.charCodeAt(i) - 97 < 0) {
-        return true;
-      }
-    }
-  });
-
-  if (hasUppercase) {
-    return "拒絕大寫";
-  }
-
   words.forEach((word) => {
     const currentScore = [...word].reduce(
-      (sum, letter) => sum + letter.charCodeAt(0) - 96,
+      // 小寫 a 的 UTF-16 編碼為 97
+      (sum, letter) => sum + letter.charCodeAt(0) - 97 + 1,
       0
     );
     if (currentScore > highestScore) {
