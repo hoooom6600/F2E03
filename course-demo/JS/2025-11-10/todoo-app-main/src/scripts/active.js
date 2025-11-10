6aa45cb07891e3a6c5ba04fe508554928c688f67
hoooom6600 <hoooom6600@gmail.com>
2025-11-10 12:41:11 +0800

    新增: 繼 2025-11-04 開發狀態，加入抓取 TODO、新 task 輸入框處理、以及簡化 axios config 參數，整理成一個新函數

---
+++ course-demo/JS/2025-11-10/todoo-app-main/src/scripts/active.js
@@ -0,0 +1,150 @@
+import axios from "axios"
+function active() {
+  return {
+    email: "",
+    nickname: "",
+    password: "",
+    newTask: "",
+    isLogin: false,
+    init() {
+      const token = localStorage.getItem("todoToken")
+      if (token) {
+        this.isLogin = true
+        // 抓 TODO
+        this.getTasks()
+      }
+      if (this.isLogin) {
+        this.goToTasks()
+      } else {
+        this.goToLogin()
+      }
+    },
+    async doLogin() {
+      const { email, password } = this
+      if (email != "" && password != "") {
+        // 登入 API，axois 用法
+        const userData = {
+          user: {
+            // 前後相同，ES6 簡寫語法
+            email,
+            password,
+          },
+        }
+        try {
+          const resp = await axios.post("https://todoo.5xcamp.us/users/sign_in", userData)
+          const token = resp.headers.authorization
+          if (token) {
+            localStorage.setItem("todoToken", token)
+          }
+          this.resetForm()
+          this.goToTasks()
+          this.isLogin = true
+        } catch (err) {
+          console.log(err)
+          alert(err.response.data.message)
+        }
+      }
+    },
+    async doSignUp() {
+      // 解構寫法
+      const { email, nickname, password } = this
+      if (email != "" && nickname != "" && password != "") {
+        // 註冊 API，axois 用法
+        const newUser = {
+          // 前後相同，ES6 簡寫語法
+          user: {
+            email,
+            nickname,
+            password,
+          },
+        }
+        try {
+          const resp = await axios.post("https://todoo.5xcamp.us/users", newUser)
+          console.log(resp)
+          this.resetForm()
+          this.goToLogin()
+        } catch (err) {
+          console.log(err)
+          alert(`${err.response.data.message}: ${err.response.data.error}`)
+        }
+      }
+    },
+    resetForm() {
+      this.email = ""
+      this.password = ""
+      this.nickname = ""
+    },
+    goToLogin() {
+      this.active = "login"
+    },
+    goToSignUp() {
+      this.active = "signup"
+    },
+    goToTasks() {
+      this.active = "tasks"
+    },
+
+    async addTask() {
+      if (this.newTask != "") {
+        const taskData = {
+          todo: {
+            content: this.newTask,
+          },
+        }
+        const config = this.getConfig()
+        const resp = await axios.post("https://todoo.5xcamp.us/todos", taskData, config)
+
+        console.log(resp)
+
+        // 新增之後，將輸入框文字清除
+        this.newTask = ""
+      }
+    },
+
+    showLogin() {
+      return this.active == "login"
+    },
+    showSignUp() {
+      return this.active == "signup"
+    },
+    showTasks() {
+      return this.active == "tasks"
+    },
+
+    async getTasks() {
+      try {
+        const config = this.getConfig()
+        const resp = await axios.get("https://todoo.5xcamp.us/todos", config)
+        console.log(resp)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    getConfig() {
+      const token = localStorage.getItem("todoToken")
+
+      return {
+        headers: {
+          accept: "application/json",
+          Authorization: token,
+        },
+      }
+    },
+    async logout() {
+      const token = localStorage.getItem("todoToken")
+      if (token) {
+        const config = this.getConfig()
+
+        try {
+          const resp = await axios.delete("https://todoo.5xcamp.us/users/sign_out", config)
+          localStorage.removeItem("todoToken")
+          this.isLogin = false
+          this.goToLogin()
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    },
+  }
+}
+export { active }
