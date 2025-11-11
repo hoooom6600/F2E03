5f545df550659c86078603e40eac03a46a49266d
hoooom6600 <hoooom6600@gmail.com>
2025-11-11 13:25:22 +0800

    新增: 糖果第 17 題實作，並作第 1 次繳交

--- HW/candy/17.js
+++ HW/candy/17.js
@@ -5,6 +5,19 @@
 
 function countBits(num) {
   // 實作寫在這裡
+  // 多少進位
+  const carry = 2;
+
+  // 進位轉換
+  const binary = num.toString(carry);
+
+  // 嘗試用 reduce 同時做過濾並累積
+  return [...binary].reduce((count, current) => {
+    if (Number(current) == 1) {
+      count++;
+    }
+    return count;
+  }, 0);
 }
 
 console.log(countBits(1234)); // 5
