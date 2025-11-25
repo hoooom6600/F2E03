e7183b6dbe1d897e4f31085848184ec11a8de2b1
hoooom6600 <hoooom6600@gmail.com>
2025-11-25 15:09:46 +0800

    新增: 糖果第 23 題實作，並做第 1 次繳交

--- HW/candy/23.js
+++ HW/candy/23.js
@@ -4,6 +4,22 @@
 
 function calcGCD(...numbers) {
   // 實作程式碼寫在這裡
+  numbers = numbers.sort((prev, next) => next - prev);
+
+  // 最大公因數不可能 > 陣列中的最小值
+  const min = Math.min(...numbers);
+
+  const factors = [];
+
+  // 類似小學找公因數的短除法，L 型的那個，只是程式寫短除法只能盡舉慢慢來
+  for (let factor = 1; factor <= min; factor++) {
+    let isdivisible = numbers.every((number) => number % factor == 0);
+    if (isdivisible) {
+      factors.push(factor);
+    }
+  }
+
+  return factors[factors.length - 1];
 }
 
 console.log(calcGCD(10)); // 10
