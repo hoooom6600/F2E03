22fee170e538f7d9c326351fcf75198def2966ea
hoooom6600 <hoooom6600@gmail.com>
2025-11-25 15:12:02 +0800

    新增: 糖果第 24, 25 題題目

---
+++ HW/candy/24.js
@@ -0,0 +1,13 @@
+// 編號：CANDY-024
+// 程式語言：JavaScript
+// 題目：算出 N 個數字的最小公倍數
+// 提示：可使用 023 計算最大公因數的函數
+
+function calcLCM(...numbers) {
+  // 實作程式碼寫在這裡
+}
+
+console.log(calcLCM(10)); // 10
+console.log(calcLCM(103, 27)); // 2781
+console.log(calcLCM(21, 15, 18)); // 630
+console.log(calcLCM(104, 96, 36, 88)); // 41184
