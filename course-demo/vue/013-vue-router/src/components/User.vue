92299a3a9a48b93b6117cf06246a62b7025b66f1
hoooom6600 <hoooom6600@gmail.com>
2025-12-01 09:38:55 +0800

    新增: vue 課堂 demo 檔案

---
+++ course-demo/vue/013-vue-router/src/components/User.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useRoute, RouterView, RouterLink } from 'vue-router'
+
+const route = useRoute()
+const id = route.params.id
+</script>
+
+<template>
+  <div>
+    <!-- <h1>User {{ id }}</h1> -->
+     <h1>User {{ $route.params.id }}</h1>
+     <RouterLink to="/user/123/profile">Profile</RouterLink>
+     <Router-view />
+  </div>
+</template>
---
+++ course-demo/vue/013-vue-router/src/components/UserProfile.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const id = route.params.id
+</script>
+
+<template>
+  <div>
+    <h1>User Profile {{ id }}</h1>
+  </div>
+</template>
