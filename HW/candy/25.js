99f183b960922ef960302e20ad1606fa7479a11a
hoooom6600 <hoooom6600@gmail.com>
2025-12-01 23:37:44 +0800

    新增: 糖果第 25 題作答到一半，尚缺有 digits 參數的處理

--- HW/candy/25.js
+++ HW/candy/25.js
@@ -13,12 +13,54 @@ function bankersRounding(num, digits = 0) {
   let integer = numString.split(".")[0];
   let decimal = numString.split(".")[1];
 
-  const targetPlaceIndex = decimal.length - (1 + digits); // 銀行家捨入法到小數點後「第幾位」
-  let targetPlace = Number(decimal[targetPlaceIndex]);
+  // 沒 digits 參數 → 捨入法到個位數
+  if (digits == 0) {
+    const roundingIndex = integer.length - 1; // 個位數
+    const decidingIndex = 0; // 小數點後第一位
+    const roundingDigit = Number(integer[roundingIndex]);
+    const decidingDigit = Number(decimal[decidingIndex]);
 
-  if (targetPlace < 5) {
-  } else if (targetPlace > 5) {
-  } else {
+    // 捨入法邏輯
+    // 四捨
+    if (decidingDigit < 5) {
+      return Number(integer);
+    }
+    // 六入
+    else if (decidingDigit > 5) {
+      return Number(integer) + 1;
+    }
+    // 五成雙
+    else {
+      if (roundingDigit % 2 == 0) {
+        return Number(integer); // 偶數，不變
+      }
+      return Number(integer) + 1; // 奇數，進位
+    }
+  }
+  // 有 digits 參數 → 捨入法到小數位
+  const roundingIndex = digits - 1;
+  const roundingDigit = Number(decimal[digits]);
+  const decidingDigit = Number(decimal[roundingIndex]);
+
+  const carryNumber = Number(`0.${"0".repeat(roundingIndex)}1`);
+
+  // console.log(num + carryNumber);
+
+  // 捨入法邏輯
+  // 四捨
+  if (decidingDigit < 5) {
+    return Number(num.toString().slice(0, -1));
+  }
+  // 六入
+  else if (decidingDigit > 5) {
+    return Number((num + carryNumber).toString().slice(0, -1));
+  }
+  // 五成雙
+  else {
+    if (roundingDigit % 2 == 0) {
+      return roundingDigit; // 偶數，不變
+    }
+    return roundingDigit + 1; // 奇數，進位
   }
 }
 
