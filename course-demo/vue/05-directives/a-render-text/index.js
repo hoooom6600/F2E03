4ce0d1af5fb09dc6059fb3aae312a8273f2c36f2
hoooom6600 <hoooom6600@gmail.com>
2025-12-05 10:09:20 +0800

    更新: 練習不同模板語法，包含嘗試讓 v-text 接受多個屬性值

--- course-demo/vue/05-directives/a-render-text/index.js
+++ course-demo/vue/05-directives/a-render-text/index.js
@@ -1,9 +1,11 @@
 Vue.createApp({
   data() {
     return {
-      studentName: '小明',
+      studentName: "小明",
+      studentNumber: 18,
+      checkInWord: "請報到",
       welcomeMessage:
-        '<h4 style="color: green">🌟 今天要學習很多有趣的 Vue 指令！</h4>'
-    }
-  }
-}).mount('#template-syntax')
+        '<h4 style="color: green">🌟 今天要學習很多有趣的 Vue 指令！</h4>',
+    };
+  },
+}).mount("#template-syntax");
