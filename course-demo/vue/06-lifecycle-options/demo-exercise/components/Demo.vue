9a2ef41394dc958ac5ddc944bab2d478297200a8
hoooom6600 <hoooom6600@gmail.com>
2025-12-08 14:09:32 +0800

    新增: 練習引入元件 + 理解生命週期

---
+++ course-demo/vue/06-lifecycle-options/demo-exercise/components/Demo.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      text: "Hello12312",
+    };
+  },
+  beforeCreate() {
+    this.text = "HIIIIII WOLRD";
+  },
+  created() {
+    console.log(`可以看到 this.text → ${this.text}，但不會是 HIIIIII WOLRD`);
+  },
+};
+</script>
+
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<style></style>
---
+++ course-demo/vue/06-lifecycle-options/demo-exercise/src/App.vue
@@ -0,0 +1,27 @@
+<!-- 元件引入組合式語法 -->
+<!-- <script setup>
+import Demo from "../components/Demo.vue";
+</script> -->
+
+<!-- 元件引入選項式語法 -->
+<script>
+import Demo from "../components/Demo.vue";
+
+export default {
+  components: {
+    Demo,
+  },
+};
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <Demo />
+  <p>
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
+  </p>
+</template>
+
+<style scoped></style>
