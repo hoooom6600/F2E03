3f06f39b88f1c744c9d6eb45955ad13d2645703b
hoooom6600 <hoooom6600@gmail.com>
2025-12-17 14:01:57 +0800

    新增: emit 練習題目

---
+++ course-demo/vue/010-component/src/components/practice_emit/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="counter">
+    <button @click="decrement">-</button>
+    <span class="count">{{ count }}</span>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  count: {
+    type: Number,
+    default: 0,
+  },
+});
+
+// TODO: 使用 defineEmits 定義 'update:count' 事件
+
+const increment = () => {
+  // TODO: emit 新的數值（count + 1）
+};
+
+const decrement = () => {
+  // TODO: emit 新的數值（count - 1），但不能小於 0
+};
+</script>
+
+<style scoped>
+.counter {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+.counter button {
+  width: 40px;
+  height: 40px;
+  font-size: 20px;
+  cursor: pointer;
+}
+.count {
+  font-size: 24px;
+  min-width: 50px;
+  text-align: center;
+}
+</style>
