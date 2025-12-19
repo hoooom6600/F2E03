04e30eace030a6ebeaf9169922486035f8916cc5
hoooom6600 <hoooom6600@gmail.com>
2025-12-19 13:35:38 +0800

    更新: 練習路由守衛 meta.requireAuth, router.beforeEach

--- course-demo/vue/013-vue-router/src/router/index.js
+++ course-demo/vue/013-vue-router/src/router/index.js
@@ -15,6 +15,10 @@ const router = createRouter({
       path: "/about",
       name: "about",
       component: () => import("../views/AboutView.vue"),
+      // 加上拜訪權限要求
+      meta: {
+        requireAuth: true,
+      },
     },
     {
       path: "/user/:id",
@@ -48,6 +52,25 @@ const router = createRouter({
       component: () => import("../components/ProductDetail.vue"),
     },
   ],
+});
+
+// 拜訪權限
+router.beforeEach((to, from, next) => {
+  const isAuth = false; // 未登入
+
+  // 檢查權限
+  if (to.meta.requireAuth) {
+    if (!isAuth) {
+      alert("未登入");
+      next("/");
+    } else {
+      next(); // 有權限，就去該去的頁面
+    }
+  } else {
+    // 這邊不 else，則其他頁面沒有權限要求就無法渲染出來
+    // 其他路由不需要授權，也必須呼叫 next() 才能渲染
+    next();
+  }
 });
 
 export default router;
